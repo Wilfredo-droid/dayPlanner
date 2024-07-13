9fab49d577fbce1036452fd139ce9545382d4947
Wilfredo-droid <wilfredoandres10@gmail.com>
2024-07-13 08:40:33 -0400

    Add general structure for moving the block in js

---
+++ script.js
@@ -0,0 +1,32 @@
+const dayBlock = document.querySelector(".dayBlock");
+const block = document.querySelector(".block");
+let isHold = true;
+let isInBlock = true;
+
+const moveBlockWhenHold = () => {
+
+/*General structure
+-Stablish a relation between dayBlock mousedown and mouseup 
+to make the event fire only when the mouse is holding the 
+block 
+
+-When the mouse is holding the block, generate an event 
+listener that moves the block withing the general dayBlock
+and remove the pointer events to avoid glitches when the
+mouse goes off the block, when the mouse stops holding, remove the event listener and reincorporate pointer events
+
+
+*/
+
+
+
+}
+
+moveBlockWhenHold();
+
+
+
+
+
+
+
